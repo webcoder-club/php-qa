91eeb1cccc780fa25daacda93d334851e758bf91
Igor Somov <isomov@avito.ru>
2018-03-22 16:42:28 +0300

    Add extra space

--- src/Day1/PrimeFactorsExample/PrimeFactors.php
+++ src/Day1/PrimeFactorsExample/PrimeFactors.php
@@ -11,6 +11,7 @@ class PrimeFactors
     public function generate(int $number): array
     {
         $primes = [];
+
         for ($candidate = 2; $number > 1; $candidate++) {
             for (; $number % $candidate == 0; $number /= $candidate) {
                 $primes[] = $candidate;
